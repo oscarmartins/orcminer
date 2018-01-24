2b016a586b9695b5aafd887476af8feea39944c5
Oscar Martins <oscarrafaelcampos@gmail.com>
2018-01-24 17:32:36 +0000

    testes

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 const http = require('http');
 const request = require('request');
-
-http.createServer(function(req, res){
-    // request new visitor
+var createHTML = require('create-html')
+function blockchain () {
     request({
         url: 'https://blockchain.info/stats?format=json',
         json: true
@@ -12,6 +11,26 @@ http.createServer(function(req, res){
         }
         console.log(body.market_price_usd)
     });
-    console.log('hi i`m a new visitor' + req.url)
-    res.end('bit coin to the moon');
+}
+
+function renderMatrix () {
+    const series = ['A', 'B', 'C', 'D', 'E']
+    var output = '<form id="inputField" role="form" autocomplete="off">';
+    for (var ui = 0; ui < series.length; ui++) {
+        output += '<input type="text" name="{{name}}" size="2" />'.replace('{{name}}', series[ui])
+    }
+    output += '</form>'
+    return output;
+}
+
+http.createServer(function(req, res){
+    // request new visitor
+    // blockchain();
+    const html = createHTML({
+        title: 'example',
+        head: '<meta name="description" content="example">',
+        body: renderMatrix(),
+        favicon: 'favicon.png'
+      })
+    res.end(html);
 }).listen(8000);
